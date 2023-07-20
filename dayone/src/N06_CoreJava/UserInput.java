7c8dda66d5d2d8f21f606e2a0b7691ffbb2d7de4
hp <hp@LAPTOP-469LARCI>
2023-07-20 23:02:29 +0530

    first commit

---
+++ dayone/src/N06_CoreJava/UserInput.java
@@ -0,0 +1,17 @@
+
+	//program to demonstrate on user-input value
+	package N06_CoreJava;
+
+	import java.util.Scanner;
+
+	public class UserInput {
+
+		public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		System.out.println("enter a string:");
+		String a=s.nextLine();
+		System.out.println(" string is "+a);
+
+		}
+
+	}
